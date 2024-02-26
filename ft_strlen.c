4c60f950ac40315592203cad4b8dcde101378195
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-26 19:42:54 +0800

    Create ft_strlen.c

---
+++ ft_strlen.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 19:07:23 by hheng             #+#    #+#             */
+/*   Updated: 2024/02/26 19:08:44 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t strlen(const char *s)
+{
+	size_t count;
+
+	count = 0;
+	while(s[count])
+	count++;
+	return(count);
+
+}
+
+int main()
+{
+	const char *str = "Hello World !";
+	size_t length = strlen(str);
+	printf("Length of the stirng '%s' is : %zu\n", str, length);
+	return 0;
+}
