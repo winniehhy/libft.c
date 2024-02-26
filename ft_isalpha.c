48d11ba3bcbd2c3fbc81223dc5a719d6396e681e
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-26 19:41:28 +0800

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -3,20 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isalpha.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/24 10:57:27 by hheng             #+#    #+#             */
-/*   Updated: 2024/02/24 10:57:27 by hheng            ###   ########.fr       */
+/*   Created: 2024/02/26 18:20:37 by hheng             #+#    #+#             */
+/*   Updated: 2024/02/26 19:12:13 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_isalpha(int c)
 {
-	return ((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
+	if	((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	return (0);
 }
-//  if (c >= 65 && c <= 90)
-// 	    return (2);
-// 	else if (c >= 97 && c <= 122)
-// 	    return (1);
-// 	else
-// 	    return (0);
+
+int main()
+{
+	char test_char = '2';
+
+	if (ft_isalpha(test_char))
+	{
+		printf("%c is an alphabetic character.\n", test_char);
+	}
+	else
+	{
+		printf("%c is not an alphabetic character.\n", test_char);
+	}
+	return 0;
+}
