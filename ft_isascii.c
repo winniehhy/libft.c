3f09663bb8f4d96132e8f66db8705c2cca82e356
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-27 16:00:10 +0800

    Update ft_isascii.c

--- ft_isascii.c
+++ ft_isascii.c
@@ -9,12 +9,24 @@
 /*   Updated: 2024/02/24 10:57:25 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
 int	ft_isascii(unsigned int a)
 {
 	return (a >= 0 && a <= 127);
 }
-// if (a >= 0 && a <= 127)
-	// 	return (1);
-	// else
-	// 	return (0);
+
+int main()
+{
+	unsigned int input = 12;
+	int result = ft_isascii(input);
+	if (result)
+	{
+		printf("%u is an ASCII character.\n", input);
+	}
+	else
+	{
+		printf("%u is not an ASCII character.\n", input);
+	}
+	return 0;
+}
