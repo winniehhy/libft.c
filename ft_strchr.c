fd676cafcc27d326050e9e8c54d74e476fcc75e7
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-28 18:17:26 +0800

    Create ft_strchr.c

---
+++ ft_strchr.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/24 10:57:06 by hheng             #+#    #+#             */
+/*   Updated: 2024/02/24 10:57:06 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strchr(const char *s, int c)
+{
+    char *s1;
+    char s2;
+    int a;
+
+    s1 = (char*)s;
+    s2 = (char)c;
+    a =0;
+    while(s1[a])
+    {
+        if (s1[a] ==s2)
+        return(&s1[a]);
+        a++;
+    }
+    if(s1[a] == s2)
+    return (&s1[a]);
+    return(NULL);
+}
+
+int main()
+{
+    const char *str = "Hello World";
+    int search_char = 'd';
+
+    char *result = ft_strchr(str, search_char);
+
+    if (result != NULL)
+    {
+        printf("Character '%c' found at position : %d\n", search_char, result - str );
+    }else{
+        printf("Character '%c' not found \n", search_char );
+    }
+    return 0;
+}
