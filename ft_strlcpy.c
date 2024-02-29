2f982ac02dbd35ebece6202c64794e00252238ea
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-02-29 18:11:47 +0800

    revise ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -10,6 +10,10 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+
+//need to revise
+
+
 #include "libft.h"
 
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
