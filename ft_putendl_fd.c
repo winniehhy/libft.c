134ca55d5b6136c766798fad13d3670825afb7f2
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-05 13:18:56 +0800

    Create ft_putendl_fd.c

---
+++ ft_putendl_fd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:16:09 by hheng             #+#    #+#             */
+/*   Updated: 2024/03/05 13:18:28 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+	write(fd, "\n", 1);
+}
+/*
+int main()
+{
+    char *message = "Hello, world!"; 
+    int fd = STDOUT_FILENO;          
+  
+    ft_putendl_fd(message, fd);
+
+    return 0;
+}
+*/
