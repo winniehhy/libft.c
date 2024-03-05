2ba36c0fe9cbc249fc50ddb7f89c5555f0f121d3
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-05 13:22:12 +0800

    Create ft_putstr_fd.c

---
+++ ft_putstr_fd.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:20:05 by hheng             #+#    #+#             */
+/*   Updated: 2024/03/05 13:21:48 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i])
+	{
+		write(fd, &s[i], 1);
+		i++;
+	}
+}
+
+/*
+int main()
+{
+    char *message = "Hello, world!"; 
+    int fd = STDOUT_FILENO;          
+
+    ft_putstr_fd(message, fd);
+
+    return 0;
+}
+*/
