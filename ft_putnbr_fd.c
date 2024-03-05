24bb3d10b121d21aaf287a085eebf797a2db7c26
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-05 13:25:36 +0800

    Create ft_putnbr_fd.c
    need to revise

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 13:22:20 by hheng             #+#    #+#             */
+/*   Updated: 2024/03/05 13:25:11 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putchar_fd('2', fd);
+		ft_putnbr_fd(147483648, fd);
+	}
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		n *= -1;
+		ft_putnbr_fd(n, fd);
+	}
+	else if (n > 9)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putnbr_fd(n % 10, fd);
+	}
+	else
+	{
+		ft_putchar_fd(n + 48, fd);
+	}
+}
+/*
+
+int main()
+{
+    int number = 12345;            
+    int fd = STDOUT_FILENO;        
+    ft_putnbr_fd(number, fd);
+
+    return 0;
+}
+*/
