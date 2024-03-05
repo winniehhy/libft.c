58772e77c77c4250d11f8b62973221ff54f2a752
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-05 09:32:41 +0800

    revised ft_calloc.c
    need to know the difference for calloc and malloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,28 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hheng < hheng@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 06:20:31 by hheng             #+#    #+#             */
-/*   Updated: 2024/02/29 06:20:31 by hheng            ###   ########.fr       */
+/*   Updated: 2024/03/05 09:31:45 by hheng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//need to revise
-
 #include "libft.h"
 
-void	*ft_calloc(size_t count, size_t size)
+void	*ft_calloc(size_t n, size_t size)
 {
 	void	*temp;
 	size_t	i;
 
 	i = -1;
-	if ((count > 65535 || size > 65535) && ((i / count) < size))
+	if ((n > 65535 || size > 65535) && ((i / n) < size))
 		return (NULL);
-	temp = malloc(count * size);
+	temp = malloc(n * size);
 	if (!temp)
 		return (NULL);
-	ft_bzero(temp, count * size);
+	ft_bzero(temp, n * size);
 	return (temp);
 }
+/*
+int main()
+{
+	size_t num_element = 10;
+	size_t element_size = sizeof(int);
+	int *arr;
+
+	arr = ft_calloc(num_element, element_size);
+
+//chech if allocation was successful
+	if (arr == NULL)
+	{
+		printf("Memory allocation failed.\n");
+	return 1;
+	}
+
+
+	printf("Allocated array :\n");
+	for (size_t i = 0; i< num_element; i++)
+{
+	printf("%d", arr[i]); // accessing element of the allocated array
+}
+	free(arr);
+	return 0;
+}
+*/
