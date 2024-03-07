ebde7203709650b8d0940774182f4fb9ca393b18
wiwi_hhy <155449474+winniehhy@users.noreply.github.com>
2024-03-07 18:07:36 +0800

    Create ft_lstmap.c

---
+++ ft_lstmap.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hheng <hheng@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/05 18:36:49 by hheng             #+#    #+#             */
+/*   Updated: 2024/03/07 18:04:19 by hheng            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_node;
+	t_list	*ret;
+
+	if (!lst)
+		return (0);
+	ret = 0;
+	while (lst)
+	{
+		new_node = ft_lstnew(f(lst->content));
+		if (!new_node)
+		{
+			ft_lstclear(&new_node, del);
+			return (0);
+		}
+		ft_lstadd_back(&ret, new_node);
+		lst = lst->next;
+	}
+	return (ret);
+}
